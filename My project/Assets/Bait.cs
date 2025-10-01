796701682783159336467a99cbb25d7840048b71
Gabriel <1721488@mukilteo.wednet.edu>
2025-10-01 09:59:43 -0700

    added bait mechanic

--- My project/Assets/Bait.cs
+++ My project/Assets/Bait.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Bait : MonoBehaviour
 {
+    public KeyCode Throw = KeyCode.F;
+    public GameObject Projectile;
+    public Transform ProjectileTransform;
+    public bool canFire;
+    private float timer;
+    public float Timebetween;
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,10 @@ public class Bait : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(Throw))
+        {
+           Instantiate(Projectile, player.transform.position, player.transform.rotation);
+            Debug.Log("Bait Thrown");
+        }
     }
 }
