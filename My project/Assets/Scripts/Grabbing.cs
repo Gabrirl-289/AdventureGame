3fbf662b0257b99ce9a67ef5775da32a88181737
Gabriel <1721488@mukilteo.wednet.edu>
2025-10-03 10:22:50 -0700

    grabbing items mechaninc

---
+++ My project/Assets/Scripts/Grabbing.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Grabbing : MonoBehaviour
+{
+    public float grabRange = 10f;
+    public KeyCode grabKey = KeyCode.E;
+    private GameObject grabbedObject;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(grabKey))
+        {
+            if (grabbedObject == null)
+            {
+                // Try to grab
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, grabRange);
+                if (hit.collider != null && hit.collider.CompareTag("weight"))
+                {
+                    grabbedObject = hit.collider.gameObject;
+                    grabbedObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic; // Disable physics while holding
+                    grabbedObject.transform.SetParent(transform);
+                    grabbedObject.transform.localPosition = new Vector3(1.5f, 0, 0); // Hold to the right of player
+                }
+            }
+            else
+            {
+                // Place object
+                grabbedObject.transform.SetParent(null);
+                grabbedObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic; // Re-enable physics
+                grabbedObject.transform.position = transform.position + transform.right * 2f; // Place 2 units to the right
+                grabbedObject = null;
+            }
+        }
+    }
+}
