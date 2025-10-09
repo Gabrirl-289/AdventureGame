9f5fc75e20339393c34b21d68ae1456c926b7107
MUKILTEO\1725203 <bnchild01@gmail.com>
2025-10-09 09:40:51 -0700

    Worked on door(did not finish)

--- My project/Assets/DoorTriggerButton.cs
+++ My project/Assets/DoorTriggerButton.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class DoorTriggerButton : MonoBehaviour
 {
-    [SerializeField] private DoorSetActive door;
+    [SerializeField] private DoorAnimated door;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
             door.OpenDoor();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            door.CloseDoor();
         }
     }
 }
