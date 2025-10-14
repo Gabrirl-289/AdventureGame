5f61b0f9df5263b5874f83b9ccc6f18e24e999d1
MUKILTEO\1726637 <1726637@mukilteo.wednet.edu>
2025-10-14 10:20:00 -0700

    shadows

--- My project/Assets/Scripts/MainTitle.cs
+++ My project/Assets/Scripts/MainTitle.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainTitle : MonoBehaviour
 {
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void exit()
     {
